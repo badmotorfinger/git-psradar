1d675d15ee2bc4f52edb03adb1a4e6fe68ecb056
Vince Panuccio <panuccio.vince@gmail.com>
2017-06-30 16:42:18 +1000

    Primitive build script

--- build.ps1
+++ build.ps1
@@ -1,1 +1,9 @@
 ﻿nuget install LibGit2Sharp -Version 0.24.0
+
+mkdir _publish
+
+Copy-Item .\Git-PsRadar.psd1 .\_publish -Force
+Copy-Item .\Git-PsRadar.psm1 .\_publish -Force
+Copy-Item .\Functions .\_publish\Functions -Force
+Copy-Item .\LibGit2Sharp.0.24.0 .\_publish -Recurse -Force
+Copy-Item .\LibGit2Sharp.NativeBinaries.1.0.185 .\_publish -Recurse -Force
